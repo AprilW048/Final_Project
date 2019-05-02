a7140e0f13b2a08b99994d1b009423caf3c041eb
Cheryl Lee <46765425+xinrongli@users.noreply.github.com>
2019-05-02 14:47:04 -0500

    Update Air Pollution.py

--- Air Pollution.py
+++ Air Pollution.py
@@ -54,3 +54,51 @@ def city_groupby(df, colname, city_name):
 
 
 # In[86]:
+if __name__ == '__main__':
+
+    Chicago_airpollution = city_groupby(airpollution, 'City', 'Chicago')
+    Chicago_airpollution.head()
+    airpollution[(airpollution['City']=='Chicago')]
+    #NYC_airpollution = city_groupby(airpollution, 'City', 'New York')
+    #NYC_airpollution.head()
+
+    #LA_airpollution = city_groupby(airpollution, 'City', 'Los Angeles')
+    #LA_airpollution.head()
+
+
+    # In[35]:
+
+
+    chicago_crime = read_indata('./Chicago_crime_2012-2017.csv')
+    chicago_crime.head()
+    ## count chicago crime
+    #chi_crime_per_month = chicago_crime[['ID', 'year', 'month']].groupby(['year', 'month']).size()
+
+
+    # In[36]:
+
+
+    generate_year_month(chicago_crime, 'Date').head()
+
+
+    # In[66]:
+
+
+    chicago_crime = chicago_crime[(chicago_crime.year>=2012) & (chicago_crime.year<2018)]
+    chi_crime_per_month = chicago_crime[['ID', 'year', 'month']].groupby(['year', 'month']).count().rename(columns={'ID':'count'})
+    chi_crime_per_month.head()
+
+
+    # In[75]:
+
+
+    pd.merge(Chicago_airpollution, chi_crime_per_month,on=['year','month'])
+
+
+    # In[76]:
+
+
+    Chicago_airpollution
+
+
+    # In[ ]:
